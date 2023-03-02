a9800f5f2fbd1fb666a9a1b0a24e144827c7bc4e
Ivan <givan502@gmail.com>
2023-03-02 20:53:57 +0300

    Add first task

---
+++ white/week_1/task_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Two integers passed via standard input, modulo not exceeding 1000.
+ * Goal: Print their sum to standard output.
+ */
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << x + y;
+    return 0;
+}
